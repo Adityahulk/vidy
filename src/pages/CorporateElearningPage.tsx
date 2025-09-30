fc37e10b94373455bf80d178687d26fca5a2415b
Adityahulk <58138716+Adityahulk@users.noreply.github.com>
2025-09-30 18:25:16 +0530

    Updated CorporateElearningPage.tsx

--- src/pages/CorporateElearningPage.tsx
+++ src/pages/CorporateElearningPage.tsx
@@ -424,6 +424,10 @@ export default function CorporateElearningPage() {
                   <CheckCircle className="w-4 h-4 text-green-400 mr-3" />
                   <span>Cultural adaptation</span>
                 </div>
+                <div className="mt-6 text-sm font-semibold text-green-400 group-hover:text-green-300 transition-colors duration-300 flex items-center">
+                  <span>Click to know more</span>
+                  <ArrowRight className="w-4 h-4 ml-2 group-hover:translate-x-1 transition-transform" />
+                </div>
               </div>
             </div>
 
