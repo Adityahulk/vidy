8ed783a379e6f86d105bd3c2a67449d9d4066006
Adityahulk <58138716+Adityahulk@users.noreply.github.com>
2025-10-09 12:32:43 +0530

    Updated api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = 'https://vidsimplifycloning-production.up.railway.app/';
 
 interface GenerationInput {
   type: 'video' | 'audio' | 'text';
